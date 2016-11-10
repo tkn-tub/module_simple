e2c3c807cae28c745f7931254a8755cb76c62ceb
Piotr Gawlowicz <gawlowicz@tu-berlin.de>
2016-11-10 11:44:21 +0100

    update

--- uniflex_module_simple/module_simple.py
+++ uniflex_module_simple/module_simple.py
@@ -2,7 +2,10 @@ import logging
 import random
 import time
 import queue
-import wishful_upis as upis
+
+from sbi.wifi.net_device import WiFiNetDevice
+from sbi.wifi.events import PacketLossEvent, SpectralScanSampleEvent
+
 from uniflex.core import modules
 from uniflex.core import exceptions
 
@@ -12,7 +15,7 @@ __version__ = "0.1.0"
 __email__ = "{gawlowicz}@tkn.tu-berlin.de"
 
 
-class SimpleModule(modules.DeviceModule):
+class SimpleModule(modules.DeviceModule, WiFiNetDevice):
     def __init__(self):
         super(SimpleModule, self).__init__()
         self.log = logging.getLogger('SimpleModule')
@@ -58,64 +61,57 @@ class SimpleModule(modules.DeviceModule):
 
     @modules.before_call(before_set_channel)
     @modules.after_call(after_set_channel)
-    @modules.bind_function(upis.wifi.radio.set_channel)
     def set_channel(self, channel, iface):
-        self.log.info("Simple Module sets channel: {} on device: {} and iface: {}".format(
-            channel, self.device, iface))
+        self.log.info(("Simple Module sets channel: {} " +
+                       "on device: {} and iface: {}")
+                      .format(channel, self.device, iface))
         self.channel = channel
         return ["SET_CHANNEL_OK", channel, 0]
 
-    @modules.bind_function(upis.wifi.radio.get_channel)
     def get_channel(self, iface):
         self.log.debug(
             "Simple Module gets channel of device: {} and iface: {}"
             .format(self.device, iface))
         return self.channel
 
-    @modules.bind_function(upis.radio.set_tx_power)
     def set_tx_power(self, power, iface):
-        self.log.debug("Simple Module sets power: {} on device: {} and iface: {}".format(
-            power, self.device, iface))
+        self.log.debug("Set power: {} on device: {} and iface: {}"
+                       .format(power, self.device, iface))
         self.power = power
         return {"SET_TX_POWER_OK_value": power}
 
-    @modules.bind_function(upis.radio.get_tx_power)
     def get_tx_power(self, iface):
         self.log.debug(
-            "Simple Module gets TX power on device: {} and iface: {}".format(self.device, iface))
+            "Simple Module gets TX power on device: {} and iface: {}"
+            .format(self.device, iface))
         return self.power
 
-    @modules.event_enable(upis.radio.PacketLossEvent)
     def packet_loss_event_enable(self):
         self._packetLossEventRunning = True
 
         while self._packetLossEventRunning:
             self.log.debug("Packet Lost")
-            event = upis.radio.PacketLossEvent()
+            event = PacketLossEvent()
             # yeld or send Event to controller
             self.send_event(event)
             time.sleep(random.uniform(0, 10))
 
-    @modules.event_disable(upis.radio.PacketLossEvent)
     def packet_loss_event_disable(self):
         self._packetLossEventRunning = False
 
-    @modules.service_start(upis.radio.SpectralScanService)
     def spectral_scan_service_start(self):
         self._spectralScanServiceRunning = True
 
         while self._spectralScanServiceRunning:
             self.log.debug("Spectral scan sample")
-            sample = upis.radio.SpectralScanSampleEvent(
+            sample = SpectralScanSampleEvent(
                 sample=random.uniform(0, 64))
             self.send_event(sample)
             time.sleep(1)
 
-    @modules.service_stop(upis.radio.SpectralScanService)
     def spectral_scan_service_stop(self):
         self._spectralScanServiceRunning = False
 
-    @modules.on_function(upis.radio.clean_per_flow_tx_power_table)
     def clean_per_flow_tx_power_table(self, iface):
         self.log.debug("clean per flow tx power table".format())
         raise exceptions.FunctionExecutionFailedException(
